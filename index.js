4b7dce577a78951d0b31341e92af65be9a2b1629
Quadas <musadaq23@hotmail.com>
2023-05-03 21:15:56 -0400

    updated index.js file

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const renderpage = require("./dist/generateHTML.js");
+// const generateHTML = require("./dist/generateHTML");
+// const generateCSS = require("./dist/generateCSS");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const team = [];
+let team = [];
 
-function addManager () {
-    console.log("build your team");
-    inquirer.prompt([   
+
+const addTeam() => {
+    inquirer
+    .prompt([
+        {
+        type: 'list',
+        name: 'memberRole',
+        message: 'would you like to add another member to your team?',
+        choices: ['Engineer', 'Intern', 'No thank you']
+        },
+    ])
+    .then(chosen => {
+        switch (chosen.memberRole) {
+            case 'Engineer':
+                addEngineer();
+                break;
+            case 'Intern':
+                addIntern();
+                break;
+            case 'No thank you':
+                default:
+                init(team);
+        }
+
+    });
+}
+
+    inquirer.prompt([ 
 {
     name: "name",
     type: "input",
@@ -27,7 +53,7 @@ function addManager () {
     type: "number",
     message: "what is manager's ID number?",
     validate: (id) => {
-        if (id) {
+        if (id && id > 0) {
             return true;
         } else {
                 return false;
@@ -52,43 +78,24 @@ function addManager () {
     name: "officeNumber",
     type: "input",
     message: "what is manager's office number?",
-},
-])
+    validate: (officeNumber) => {
+        if (officeNumber && Number.isInteger(officeNumber)) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+}]),
 
-.then((answers) => {
-    const manager = new Manager(
+    function addManager() = new Manager( 
         answers.name,
         answers.id,
         answers.email,
-        answers.officeNumber
-    );
+        answers.officeNumber);
     team.push(manager);
     addTeam();
-})
-};
 
-function addTeam() {
-    inquirer
-    .prompt([ {
-        type: 'list',
-        name: 'memberRole',
-        message: 'would you like to add another member to your team?',
-        choices: ['Engineer', 'Intern', 'No thank you']
-    }]).then(chosen => {
-        switch (chosen.memberRole) {
-            case 'Engineer':
-                addEngineer();
-                break;
-            case 'Intern':
-                addIntern();
-                break;
-            case 'No thank you':
-                buildTeam();
-                break;
-        }
 
-    });
-};
 
 function addEngineer() {
     inquirer
@@ -111,7 +118,7 @@ function addEngineer() {
             name: 'id',
             message: 'Enter ID of the Engineer',
             validate: (id) => {
-                if (id) {
+                if (id > 0) {
                     return true;
                 } else {
                         return false;
@@ -156,8 +163,8 @@ function addIntern() {
         .prompt([
             {
             type: 'input',
-            name: 'Name',
-            message: 'Enter the name of your Itern',
+            name: 'name',
+            message: 'Enter the name of your Intern',
             },
             {
             type: 'number',
@@ -173,20 +180,11 @@ function addIntern() {
             {
             type: 'input',
             name: 'school',
-            message: 'Enter the name of the school for your intern',
+            message: 'Enter the name of the school for your Intern',
             }
         ]).then(answers => {
             const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
             team.push(Intern);
             addTeam();
-        })   
-};
-
-function buildTeam() {
-    fs.writeFileSync('./dist/generateHTML.js',renderpage(team), 'UTF-8');
-}
-
-addManager();
-
-
-
+        })
+    };
